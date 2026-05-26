37273da85e78fba137ba892a98cbb5bb6ea45335
Chaiwat-Setho <youicesd@gmail.com>
2026-05-26 09:38:23 +0700

    fix: import @fontsource IBM Plex CSS so brand fonts load

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Providers } from './app/providers'
+import '@fontsource/ibm-plex-sans-thai/400.css'
+import '@fontsource/ibm-plex-sans-thai/500.css'
+import '@fontsource/ibm-plex-sans-thai/600.css'
+import '@fontsource/ibm-plex-sans-thai/700.css'
+import '@fontsource/ibm-plex-mono/400.css'
+import '@fontsource/ibm-plex-mono/500.css'
+import '@fontsource/ibm-plex-mono/600.css'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
